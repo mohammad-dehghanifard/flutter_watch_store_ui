b560b913f4f3e0630f1b3c733364245c8ce33059
mohammad <m.deheghanifard@gmail.com>
2024-04-23 16:41:11 +0330

    impl product model class

---
+++ lib/models/product_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ProductModel {
+  ProductModel({required this.title,required this.strap,required this.price,required this.color});
+  String? title,strap;
+  Color? color;
+  int? price;
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,14 +6,43 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              // app bar
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Image.asset("assets/images/menu.png"),
+                  Stack(
+                    children: [
+                      Image.asset("assets/images/Bag.png"),
+                      Positioned(
+                        right: 0,
+                        child: Container(
+                          width: 17,
+                          height: 17,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: const Color(0xFFFF3D71),
+                            border: Border.all(color: Colors.white,width: 3)
+                          ),
+                          child: const Center(child: Text("3",style: TextStyle(color: Colors.white,fontSize: 8),)),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(height: 48),
 
+              const Text("The Collection",style: TextStyle(fontSize: 32,color: Color(0xFF222B45)))
             ],
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
